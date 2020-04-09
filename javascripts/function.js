25f449a711802f0b5b609bf21fa356c05192ae90
Jille Bonfemme <jilleb@jakku.edu>
2020-04-09 12:23:32 -0700

    Add L.O.s to README.md

--- javascripts/function.js
+++ javascripts/function.js
@@ -4,11 +4,35 @@
 let arrAvg = arr => {
   let sum = 0;
   for (var i = 0; i < arr.length; i++) {
-    //arr[i]
+    sum;
   }
 };
 
 //Req.3c
-let arrMax = arr => {
-  //
+let arrMax = (numbers) => {
+  let currentMaximum = -Infinity;
+  numbers.forEach(function(numbers){
+    if (number > currentMaximum) {
+      currentMaximum = number;
+    }
+  });
+  return currentMaximum;
 };
+  //
+  arr.forEach((elem)
+result = "${elem} ${result} ";
+});
+
+return result;
+}
+let ;
+
+//Req.3d
+let sumevens = arr => {
+  let sum = 0;
+  for (sum of even) {
+    sum += even;
+  }
+};
+
+sum / temps.length;
