2c5eda6fc6c3d0d83c38c76bcbc991e9d60793b1
Jille Bonfemme <jilleb@jakku.edu>
2020-04-10 15:21:54 -0700

    arravg to function.js

--- javascripts/function.js
+++ javascripts/function.js
@@ -4,35 +4,27 @@
 let arrAvg = arr => {
   let sum = 0;
   for (var i = 0; i < arr.length; i++) {
-    sum;
+    sum = sum + arr[i];
   }
+  return sum / arr.length;
 };
 
 //Req.3c
-let arrMax = (numbers) => {
+let arrMax = numbers => {
   let currentMaximum = -Infinity;
-  numbers.forEach(function(numbers){
+  numbers.forEach(function(numbers) {
     if (number > currentMaximum) {
       currentMaximum = number;
     }
   });
   return currentMaximum;
 };
-  //
-  arr.forEach((elem)
-result = "${elem} ${result} ";
-});
-
-return result;
-}
-let ;
 
 //Req.3d
-let sumevens = arr => {
+let sumEvens = arr => {
   let sum = 0;
   for (sum of even) {
     sum += even;
   }
+  console.log(sumEvens);
 };
-
-sum / temps.length;
